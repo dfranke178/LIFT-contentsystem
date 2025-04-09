a07119e2c4ec2ce4abecb426a0fe1ea16df65e7a
David Franke <david@liftleadershipgroup.com>
2025-04-09 14:16:19 -0500

    Fix circular imports in src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,23 @@
 """
-LinkedIn Content Analysis System
+Main package for the LinkedIn Content System
 """
 
-import os
-import sys
-
-# Only import data processor if not running tests
-if not any(arg.endswith('pytest') for arg in sys.argv):
-    from src.data_processor import DataProcessor
-
-# Version information
-__version__ = "1.0.0"
-
-# Make functions available for import
-from src.utils import save_json, create_timestamp, get_posts_for_training
+from src.utils.data_utils import (
+    get_posts_for_training,
+    save_json,
+    load_json,
+    create_timestamp,
+    ensure_directory
+)
 from src.utils.feedback_loop import FeedbackLoop
+from src.data_processor import DataProcessor
 
-__all__ = ['DataProcessor', 'save_json', 'create_timestamp', 'FeedbackLoop']
+__all__ = [
+    'get_posts_for_training',
+    'save_json',
+    'load_json',
+    'create_timestamp',
+    'ensure_directory',
+    'FeedbackLoop',
+    'DataProcessor'
+]
